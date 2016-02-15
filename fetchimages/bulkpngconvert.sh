2df2e7f76cb0eccbb7b556b13ea66ad84022a55e
David McInnis <davidm@eagles.ewu.edu>
2016-02-15 14:59:54 -0800

    changed bulkconvert to compress binary files instead of expand them.

--- fetchimages/bulkpngconvert.sh
+++ fetchimages/bulkpngconvert.sh
@@ -10,7 +10,13 @@ for i in $(find . -maxdepth 1 -type d); do
     if [ ${i:3} ]; then
         cd ${i:2}"/otiff"
         for j in $(ls *.tif); do
-            convert -verbose -quality 9 $j ${j:0:8}".png"
+            jid=$(identify $j | grep '8-bit')
+            if [ ${jid:3} ]; then
+                convert -verbose -quality 9 $j ${j:0:8}".png"
+
+            else
+                convert -verbose -quality 9 -colors 4 $j ${j:0:8}".png"
+            fi
             rm $j
         done
         cd $START_DIR
