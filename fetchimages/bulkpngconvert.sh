0db5e0da5e60b433869d17fb649dee70caa686c6
David McInnis <davidm@eagles.ewu.edu>
2016-02-15 15:27:12 -0800

    added more messages.

--- fetchimages/bulkpngconvert.sh
+++ fetchimages/bulkpngconvert.sh
@@ -10,6 +10,7 @@ for i in $(find . -maxdepth 1 -type d); do
     if [ ${i:3} ]; then
         cd ${i:2}"/otiff"
         for j in $(ls *.tif); do
+            echo "identify: "$j
             jid=$(identify $j | grep '8-bit')
             if [ ${jid:3} ]; then
                 convert -verbose -quality 9 $j ${j:0:8}".png"
