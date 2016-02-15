ee34546c26817494d925c199b03f86003531a161
David McInnis <davidm@eagles.ewu.edu>
2016-02-15 12:07:13 -0800

    fixed typo

--- fetchimages/bulkpngconvert.sh
+++ fetchimages/bulkpngconvert.sh
@@ -10,7 +10,6 @@ for i in $(find . -maxdepth 1 -type d); do
     if [ ${i:3} ]; then
         cd ${i:2}"/otiff"
         for j in $(ls *.tif); do
-            echo $j
             convert -quality 9 $j {j:0:8}".png"
             rm $j
         done
