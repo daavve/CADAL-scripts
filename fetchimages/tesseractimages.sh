f49db2202724e70364bfab336ca1c8ff0a633e93
David McInnis <davidm@eagles.ewu.edu>
2016-02-14 16:06:54 -0800

    got tesseract script up and running

--- fetchimages/tesseractimages.sh
+++ fetchimages/tesseractimages.sh
@@ -6,11 +6,19 @@
 
 cd "www.cadal.zju.edu.cn/CalliSources/booksserve"
 START_DIR=$(pwd)
-for i in $(find . -type d -maxdepth 1)
-do
+for i in $(find . -type d -maxdepth 1); do
     if [ ${i:3} ]; then
         cd ${i:2}"/otiff"
-        pwd
+        if [ -e 00000001.tif ]; then
+            for j in $(ls *.tif); do
+                tesseract $j -l chi_tra stdout | tee ${j:0:8}".txt"
+            done
+        else
+            for j in $(ls *.jpg); do
+                tesseract $j -l chi_tra stdout | tee ${j:0:8}".txt"
+            done
+        fi
+
         cd $START_DIR
     fi
 done
