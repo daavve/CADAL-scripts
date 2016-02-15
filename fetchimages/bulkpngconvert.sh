799159389827075eeaf52ed9a4a6594d0f22d2c0
David McInnis <davidm@eagles.ewu.edu>
2016-02-15 12:37:44 -0800

    fixed where we save to the wrong location

--- fetchimages/bulkpngconvert.sh
+++ fetchimages/bulkpngconvert.sh
@@ -4,13 +4,13 @@
 #
 ##########################################
 
-cd "www.cadal.zju.edu.cn/CalliSources/books"
+cd "www.cadal.zju.edu.cn/CalliSources/books_local"
 START_DIR=$(pwd)
 for i in $(find . -maxdepth 1 -type d); do
     if [ ${i:3} ]; then
         cd ${i:2}"/otiff"
         for j in $(ls *.tif); do
-            convert -quality 9 $j {j:0:8}".png"
+            convert -verbose -quality 9 $j ${j:0:8}".png"
             rm $j
         done
         cd $START_DIR
