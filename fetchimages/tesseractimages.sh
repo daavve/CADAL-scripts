57340ee3d5062cea9924750b358cc5a2d68b7dab
David McInnis <davidm@eagles.ewu.edu>
2016-02-15 10:55:06 -0800

    works on all but the mixed jpeg/tiff dirs

--- fetchimages/tesseractimages.sh
+++ fetchimages/tesseractimages.sh
@@ -4,17 +4,19 @@
 #
 ####################################
 
-cd "www.cadal.zju.edu.cn/CalliSources/books"
+cd "www.cadal.zju.edu.cn/CalliSources/booksserve"
 START_DIR=$(pwd)
-for i in $(find . -type d -maxdepth 1); do
+for i in $(find . -maxdepth 1 -type d); do
     if [ ${i:3} ]; then
         cd ${i:2}"/otiff"
         if [ -e 00000001.tif ]; then
             for j in $(ls *.tif); do
+                echo $j
                 tesseract $j -l chi_tra stdout | tee ${j:0:8}".txt"
             done
         else
             for j in $(ls *.jpg); do
+                echo $j
                 tesseract $j -l chi_tra stdout | tee ${j:0:8}".txt"
             done
         fi
