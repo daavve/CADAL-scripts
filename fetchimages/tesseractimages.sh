6a5ddca833ae479c40aec8ac4f92458ee3b00df0
David McInnis <davidm@eagles.ewu.edu>
2016-02-16 07:00:40 -0800

    just doing jpeg files for now.

--- fetchimages/tesseractimages.sh
+++ fetchimages/tesseractimages.sh
@@ -4,23 +4,17 @@
 #
 ####################################
 
-cd "www.cadal.zju.edu.cn/CalliSources/booksserve"
+cd "www.cadal.zju.edu.cn/CalliSources/books_local"
 START_DIR=$(pwd)
 for i in $(find . -maxdepth 1 -type d); do
     if [ ${i:3} ]; then
         cd ${i:2}"/otiff"
-        if [ -e 00000001.tif ]; then
-            for j in $(ls *.tif); do
-                echo $j
+        for j in $(ls *.jpg); do
+            echo $j
+            if [ ! -f ${j:0:8}".txt" ]; then
                 tesseract $j -l chi_tra stdout | tee ${j:0:8}".txt"
-            done
-        else
-            for j in $(ls *.jpg); do
-                echo $j
-                tesseract $j -l chi_tra stdout | tee ${j:0:8}".txt"
-            done
-        fi
-
+            fi
+        done
         cd $START_DIR
     fi
 done
