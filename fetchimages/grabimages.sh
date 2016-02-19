edbb6de44817705285f95a82c7b63a158233bebb
David McInnis <davidm@eagles.ewu.edu>
2016-02-18 20:20:53 -0800

    think grab missing char images working

--- fetchimages/grabimages.sh
+++ fetchimages/grabimages.sh
@@ -5,15 +5,29 @@
 ############################
 
 
-filelist=$(find www.cadal.zju.edu.cn/ -name "downloadlist.txt")
+CHARS_DIR="/home/dave/workspace/pycharm/fetch/chars/"
+BOOK_DIR="/home/dave/workspace/pycharm/fetch/grabbedBooks/grabbedBooks/"
+WEBSITE="http://www.cadal.zju.edu.cn/CalliSources/books/"
+WEBBACKUP="http://www.cadal.zju.edu.cn/CalliSources/images/books/"
 
-for file in $filelist; do
-    filedir=${file::55}
-    weblist=$(cat $file)
-    for web in $weblist; do
-        outfile=$filedir${web:62}
-        if [ ! -e $outfile ]; then
-            wget --output-document=$outfile $web
+cd $CHARS_DIR
+
+for dir in $(ls -d */); do
+    for file in $(ls $dir); do
+        fileid=${file::8}
+        localfile=$BOOK_DIR${dir:0:8}"-"$fileid
+        localfilejpg=$localfile".jpg"
+        localfiletif=$localfile".tif"
+        if [[ -e $localfilejpg || -e $localfiletif ]]; then
+            echo "Have: "$localfile
+        else
+            webbook=$WEBSITE$dir"otiff"$fileid
+            webbooktif=$webbook".tif"
+            webbookjpg=$webbook".jpg"
+            wget $webbooktif -O $localfiletif
+            if [[ $? != 0 ]]; then
+                wget $webbookjpg -O $localfilejpg
+            fi
         fi
     done
 done
