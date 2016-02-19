4d9c992496c3e7c08d3aab46156788960459cf28
David McInnis <davidm@eagles.ewu.edu>
2016-02-18 22:36:23 -0800

    Think I fixed the script.  Also deletes all the zero-length files

--- fetchimages/grabimages.sh
+++ fetchimages/grabimages.sh
@@ -10,6 +10,12 @@ BOOK_DIR="/home/django/CADAL-scripts/fetchimages/workslist/grabbedBooks/"
 WEBSITE="http://www.cadal.zju.edu.cn/CalliSources/books/"
 WEBBACKUP="http://www.cadal.zju.edu.cn/CalliSources/images/books/"
 
+emptyfiles=$(cat emptyImages.txt)
+for file in $emptyfiles; do
+    rm -f $BOOK_DIR$file
+done
+
+
 cd $CHARS_DIR
 
 for dir in $(ls -d */); do
@@ -21,13 +27,17 @@ for dir in $(ls -d */); do
         if [[ -e $localfilejpg || -e $localfiletif ]]; then
             x=1
         else
-            webbook=$WEBSITE$dir"otiff"$fileid
+            webbook=$WEBSITE$dir"otiff/"$fileid
+            backupsite=$WEBBACKUP$dir$fileid".jpg"
             webbooktif=$webbook".tif"
             webbookjpg=$webbook".jpg"
             wget $webbooktif -O $localfiletif &> /dev/null
             if [[ $? != 0 ]]; then
                 rm $localfiletif
                 wget $webbookjpg -O $localfilejpg &> /dev/null
+                if [[ $? != 0 ]]; then
+                    wget $backupsite -O $localfilejpg &> /dev/null
+                fi
             fi
         fi
     done
