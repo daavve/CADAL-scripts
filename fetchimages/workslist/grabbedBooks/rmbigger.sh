aa25f40a3f08621c8002639e457a32484e7add7c
David McInnis <davidm@eagles.ewu.edu>
2016-02-22 10:57:09 -0800

    debug

--- fetchimages/workslist/grabbedBooks/rmbigger.sh
+++ fetchimages/workslist/grabbedBooks/rmbigger.sh
@@ -9,9 +9,9 @@ pnglist=$(ls *.png)
 
 for pngfile in $pnglist; do
     let lenT=${#pngfile}-4
-    tiffile=${pngfile:0:$lenT}".png"
-    tifSize=$(stat -c %s $tifFile)
-    pngSize=$(stat -c %s $pngFile)
+    tiffile=${pngfile:0:$lenT}".tif"
+    tifSize=$(stat -c %s $tiffile)
+    pngSize=$(stat -c %s $pngfile)
     echo $pngfile":"$tiffile
 
 #    if [[ $tifSize > $pngSize ]]; then
