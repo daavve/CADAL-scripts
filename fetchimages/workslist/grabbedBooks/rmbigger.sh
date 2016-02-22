f15ca5e7de283debfe1f31581bc6e1f743615b32
David McInnis <davidm@eagles.ewu.edu>
2016-02-22 10:55:40 -0800

    debug

--- fetchimages/workslist/grabbedBooks/rmbigger.sh
+++ fetchimages/workslist/grabbedBooks/rmbigger.sh
@@ -12,9 +12,11 @@ for pngfile in $pnglist; do
     tiffile=${pngfile:0:$lenT}".png"
     tifSize=$(stat -c %s $tifFile)
     pngSize=$(stat -c %s $pngFile)
-    if [[ $tifSize > $pngSize ]]; then
-        rm -f $tifFile
-    else
-        rm -f $pngFile
-    fi
+    echo $pngfile":"$tiffile
+
+#    if [[ $tifSize > $pngSize ]]; then
+#        rm -f $tifFile
+#    else
+#        rm -f $pngFile
+#    fi
 done
