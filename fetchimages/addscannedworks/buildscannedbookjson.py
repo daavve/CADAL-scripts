45bd4941a58e4470688fcea76eb62acaeb137fa6
David McInnis <davidm@eagles.ewu.edu>
2016-02-26 18:20:01 -0800

    fixed error where we got the wrong image.

--- fetchimages/addscannedworks/buildscannedbookjson.py
+++ fetchimages/addscannedworks/buildscannedbookjson.py
@@ -4,14 +4,8 @@
 #
 ################################################################
 
-import socket
 import json
 import os
-
-if socket.gethostname() == 'bigArch':
-    SCANNED_DIR = "/home/dave/workspace/pycharm/fetch/scanned/1/"
-else:
-    SCANNED_DIR = "/media/scanned/1/"
 
 
 class Work(object):
@@ -34,22 +28,22 @@ def jdefault(o):    # This part is necessary to get the objects to Auto-Format
 
 
 def dumptojson() -> None:
-    dumpfile = open('scannedwork1.json', mode='w')
+    dumpfile = open('c-scannedwork-1.json', mode='w')
     json.dump(wk, dumpfile, ensure_ascii=False, indent=4, sort_keys=True, default=jdefault)
     dumpfile.close()
 
+
+TOP_PATH = "scanned/1/"
 # 3109 is next available primary key in works
 wk = Work(wkid=int(4109), title="集字聖教序, 三島", author="王羲之")
-
 file = open("tesseract_convert.txt", mode="r", encoding="utf-8")
 redfile = file.read()
 file.close()
-
 textbody = redfile.split('##########################################\n\n')[1]
 textbody = textbody.split('\n\n\nSTOP!')[0]
 textlist = textbody.split('\n\n-----------------------\n')
 for text in textlist:
-    filepath = SCANNED_DIR + text.split('\n')[0]
+    filepath = TOP_PATH + text.split('\n')[0].replace('t', 'i', 1)
     if not os.path.isfile(filepath):
         Exception("File not found", filepath)
     filetext = text.split('.png\n')[1]
